7d6a105a94f56a5a02938afdafd88236dec5e4f0
Sahil Guleria <60337243+Sahilguleria12@users.noreply.github.com>
2021-07-24 09:43:20 +0530

    Update LearningOptions.jsx

--- src/bots/docsbot/widgets/docs/vttdocs/components/LearningOptions/LearningOptions.jsx
+++ src/bots/docsbot/widgets/docs/vttdocs/components/LearningOptions/LearningOptions.jsx
@@ -1,1 +1,39 @@
 
+import React from "react";
+
+import "./LearningOptions.css";
+
+const LearningOptions = (props) => {
+  const options = [
+    {
+      text: "Aarogya Care",
+      handler: props.actionProvider.handleJavascriptList,
+      id: 1,
+    },
+    { 
+      text: "Cancer", 
+      handler: props.actionProvider.handleJavascriptList1, 
+      id: 2 
+    },
+    { 
+      text: "Cholesterol", 
+      handler: props.actionProvider.handleJavascriptList2, 
+      id: 3 
+    },
+    
+  ];
+
+  const optionsMarkup = options.map((option) => (
+    <button
+      className="learning-option-button"
+      key={option.id}
+      onClick={option.handler}
+    >
+      {option.text}
+    </button>
+  ));
+
+  return <div className="learning-options-container">{optionsMarkup}</div>;
+};
+
+export default LearningOptions;
